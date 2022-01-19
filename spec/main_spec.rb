0314da864ab10318b54e9919d2dea833580cc3d5
Ayuri Bessa <ayuribessa@gmail.com>
2022-01-19 07:58:34 -0300

    set identation for 2 spaces, fix describe ad contexts

--- spec/main_spec.rb
+++ spec/main_spec.rb
@@ -1,18 +1,23 @@
 require_relative "../lib/parser"
 
-describe Parser do
+describe '#open_file' do
 
-	context " When testing the Parser classe" do
-	  it " Should print an error message if user enter wrong file name or path, " do
+	context " When loaded wrong file name or path " do
+	  it "Print an error message " do
 	      parser = Parser.new()
 	      expect{ parser.open_file('wrong_file.log')}.to raise_error("File not found")
 	  end
 
-     it "Should print this text in the first line " do
-         parser = Parser.new()
-         parser.open_file('games.log')
-         first_line = parser.print_first_line
-         expect(first_line).to eq("  0:00 ------------------------------------------------------------")
+  end
+  
+describe '#print_first_line' do
+  context "when priting the first line of the file" do
+
+    it "Print this text " do
+        parser = Parser.new()
+        parser.open_file('games.log')
+        first_line = parser.print_first_line
+        expect(first_line).to eq("  0:00 ------------------------------------------------------------")
 	  end
    end
 end
