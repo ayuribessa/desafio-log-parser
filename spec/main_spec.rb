134ae0d07798f8707bd765083c78df0737e1c129
Ayuri Bessa <ayuribessa@gmail.com>
2022-01-19 08:10:11 -0300

    fix errors

--- spec/main_spec.rb
+++ spec/main_spec.rb
@@ -7,8 +7,8 @@ describe '#open_file' do
 	      parser = Parser.new()
 	      expect{ parser.open_file('wrong_file.log')}.to raise_error("File not found")
 	  end
-
   end
+end
   
 describe '#print_first_line' do
   context "when priting the first line of the file" do
@@ -19,6 +19,6 @@ describe '#print_first_line' do
         first_line = parser.print_first_line
         expect(first_line).to eq("  0:00 ------------------------------------------------------------")
 	  end
-   end
+  end
 end
 
