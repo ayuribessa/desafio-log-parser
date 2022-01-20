882373e6d808ef0a65bff47132bd8b8a41048bc1
Ayuri Bessa <ayuribessa@gmail.com>
2022-01-19 22:20:25 -0300

    fix identation

--- spec/main_spec.rb
+++ spec/main_spec.rb
@@ -2,7 +2,7 @@ require_relative "../lib/parser"
 
 describe '#open_file' do
   context " When loaded wrong file name or path " do
-	  it "Print an error message " do
+    it "Print an error message" do
       expect{Parser.new('wrong_file.log')}.to raise_error(Errno::ENOENT)
     end
   end
@@ -14,7 +14,7 @@ describe '#print_first_line' do
       parser = Parser.new('games.log')
       first_line = parser.print_first_line
       expect(first_line).to eq("  0:00 ------------------------------------------------------------")
-	  end
+    end
   end
 end
 
