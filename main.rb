fa1d9ca97dc142730502b3cfe8878bead539efe2
AyuriBs <54184471+ayuribessa@users.noreply.github.com>
2022-01-25 11:35:05 -0300

    Update main.rb
    Co-authored-by: João Pedro <pedrochaveslimas3@gmail.com>

--- main.rb
+++ main.rb
@@ -7,7 +7,6 @@ OptionParser.new do |opts|
   opts.banner = 'Usage: main.rb [options]'
   opts.on('-n FILENAME', '--file-name FILENAME', 'Log file to parse') { |f| option[:file_name] = f }
 end
-file_name = option[:file_name]
-parser = Parser.new(file_name)
+parser = Parser.new(option[:file_name])
 puts parser.json_format
 parser.close_file
